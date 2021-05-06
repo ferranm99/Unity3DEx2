b4ce3ba87d3ce314e429df7aea6a40d648038197
ferranm99 <63805385+ferranm99@users.noreply.github.com>
2021-05-06 10:36:20 +0200

    more

---
+++ Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Copyright (c) 2019 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Sheep : MonoBehaviour
+{
+    public float runSpeed;
+    public float gotHayDestroyDelay;
+    public float heartOffset;
+    public float dropDestroyDelay;
+
+    public GameObject heartPrefab;
+
+    private Collider myCollider;
+    private Rigidbody myRigidbody;
+    private bool hitByHay;
+    private SheepSpawner sheepSpawner;
+
+    private void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+    }
+
+    public void SetSpawner(SheepSpawner spawner)
+    {
+        sheepSpawner = spawner;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hay") && !hitByHay)
+        {
+            Destroy(other.gameObject);
+            HitByHay();
+        }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+    }
+
+    private void Drop()
+    {
+        myRigidbody.isKinematic = false;
+        myCollider.isTrigger = false;
+        GameStateManager.Instance.DroppedSheep();
+        Destroy(gameObject, dropDestroyDelay);
+        sheepSpawner.RemoveSheepFromList(gameObject);
+        SoundManager.Instance.PlaySheepDroppedClip();
+    }
+
+    private void HitByHay()
+    {
+        hitByHay = true;
+        runSpeed = 0;
+
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+
+        TweenScale tweenScale = gameObject.AddComponent<TweenScale>();
+        tweenScale.targetScale = 0;
+        tweenScale.timeToReachTarget = gotHayDestroyDelay;
+
+        GameStateManager.Instance.SavedSheep();
+        Destroy(gameObject, gotHayDestroyDelay);
+        sheepSpawner.RemoveSheepFromList(gameObject);
+
+        SoundManager.Instance.PlaySheepHitClip();
+    }
+}
